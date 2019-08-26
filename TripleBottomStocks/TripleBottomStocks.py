86aec5bfcd4df83d17be5d20c8da8b3876b9afb4
DandaRaviteja <dandaraviteja@outlook.com>
2019-08-26 18:46:38 +0530

    Update TripleBottomStocks.py

--- TripleBottomStocks/TripleBottomStocks.py
+++ TripleBottomStocks/TripleBottomStocks.py
@@ -2,18 +2,22 @@ import pandas as pd
 from nsetools import Nse
 nse = Nse()
 selected_stocks=[]
-
 link = 'https://chartink.com/eodscanner/Triple-Bottom.html'
 dfs = pd.read_html(link)
 df = pd.concat(dfs)
 df = (df[1].iloc[3:]).tolist()
 stocks = [x for x in df if x != 'nan']
-
-#get all stock codes
 all_stock_codes = nse.get_stock_codes()
-for eachStock in stocks:
-        for Symbol, Name in all_stock_codes.items():
-                if str(eachStock) == Name:
-                    selected_stocks.append(Symbol)
-print("tripleBottom --> "+str(selected_stocks))
 
+selected_stocks=[]
+
+for eachStock in stocks:
+    for Key, Value in all_stock_codes.items():
+            if str(eachStock) == Value:
+                    selected_stocks.append(Key)
+            else:
+                if str(eachStock) not in all_stock_codes.values():
+                    selected_stocks.append(str(eachStock))
+                    break;
+selected_stocks = [incom for incom in selected_stocks if str(incom) != 'nan']
+print('TBL -->'+str(selected_stocks))
